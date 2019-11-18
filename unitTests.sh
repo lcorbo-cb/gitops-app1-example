ba46f24405d8e26ab79f2dc807abd5fc2bb6d374
Louie Corbo <lcorbo@cloudbees.com>
2019-11-17 19:33:06 -0600

    testing linting

--- unitTests.sh
+++ unitTests.sh
@@ -1,3 +1,7 @@
 Test01=$(hadolint --version)
 Expect01="Haskell Dockerfile Linter 1.17.2-no-git"
-if [ $Test01 -ne $Expect01 ]; then  exit 1
+if [ $Test01 -ne $Expect01 ]; then
+  echo "Test failed Expected: $Expect01"
+  echo "Recieved: $Test01"
+  exit 1
+fi
