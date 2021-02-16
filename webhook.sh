749d9466da6fc3fec4e46e665a7ecdb0abacf9a6
lcorbo-cb <lcorbo@cloudbees.com>
2021-02-15 20:50:08 -0500

    webhook works

--- webhook.sh
+++ webhook.sh
@@ -3,4 +3,6 @@ curl -X POST \
      -H "Content-Type: application/json" \
      -H "x-hub-signature:a9e49ad0fa18bfe6e8d548e739f30cb0ff7b4c3ac24753f8399551613d2f1237" \
      -d @hook.json \
+     --user admin:welcome \
+    --write-out "%{http_code}" \
      https://cbci.cloudbees.demo/cjoc/webhooks/zNJv3nw/
