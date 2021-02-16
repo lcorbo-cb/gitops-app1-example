661c3dccc2449a18b0eed6ceab291f561daa21a3
lcorbo-cb <lcorbo@cloudbees.com>
2021-02-15 19:58:05 -0500

    adding external webhook

--- webhook.sh
+++ webhook.sh
@@ -1,6 +1,6 @@
 curl -X POST \
-     -H "Content-Type: application/json" \
-     -H "x-hub=signature:" \
      --cacert ../sh_secrets_ps/certs/ca/ca.pem \
+     -H "Content-Type: application/json" \
+     -H "x-hub-signature:4e75e597d118b9c57823f85143a49e63e1701a1f88134be7e0b7698e79eba562" \
      -d @hook.json \
      https://cbci.cloudbees.demo/cjoc/webhooks/HzZDDvY/
